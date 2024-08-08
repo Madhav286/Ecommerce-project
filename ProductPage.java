f8fbe3717bc0258a06e37b360dbc184cee978f79
RATNALA BINDU MADHAV <164131589+Madhav286@users.noreply.github.com>
2024-08-08 10:54:28 +0530

    Update ProductPage.java

--- ProductPage.java
+++ ProductPage.java
@@ -1,5 +1,19 @@
 package Bestbuy;
 
-public class ProductPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class ProductPage extends BasePage {
+
+    @FindBy(css = ".add-to-cart-button")
+    private WebElement addToCartButton;
+
+    public ProductPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void addItemToCart() {
+        addToCartButton.click();
+    }
 }
