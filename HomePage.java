a5e43f5dc35bdaa4d0e7007449ab1eca8bef476d
RATNALA BINDU MADHAV <164131589+Madhav286@users.noreply.github.com>
2024-08-08 10:53:06 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,5 +1,38 @@
 package Bestbuy;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class HomePage extends BasePage {
+
+    @FindBy(id = "gh-search-input")
+    private WebElement searchInput;
+
+    @FindBy(css = ".header-search-button")
+    private WebElement searchButton;
+
+    @FindBy(css = "footer a")
+    private List<WebElement> bottomLinks;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void open(String url) {
+        driver.get(url);
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    public void searchForItem(String item) {
+        searchInput.sendKeys(item);
+        searchButton.click();
+    }
+
+    public List<WebElement> getBottomLinks() {
+        return bottomLinks;
+    }
 }
