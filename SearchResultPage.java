b0dfb6fe19f7914964481680967ea76c5e7d153f
RATNALA BINDU MADHAV <164131589+Madhav286@users.noreply.github.com>
2024-08-08 10:55:39 +0530

    Update SearchResultPage.java

--- SearchResultPage.java
+++ SearchResultPage.java
@@ -1,5 +1,21 @@
 package Bestbuy;
 
-public class SearchResultPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+public class SearchResultPage extends BasePage {
+
+    @FindBy(css = ".sku-title a")
+    private WebElement firstItemLink;
+
+    public SearchResultPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickFirstItem() {
+        wait.until(ExpectedConditions.visibilityOf(firstItemLink));
+        firstItemLink.click();
+    }
 }
